4525d3d861a36ffb182d2b6a56f909ceb5648edf
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2022-03-30 16:10:07 +0200

    Add files via upload

---
+++ server/Karte.php
@@ -0,0 +1,27 @@
+   <?php
+<style>
+      .map {
+        height: 400px;
+        width: 100%;
+      }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.13.0/build/ol.js"></script>
+        <h2>My Map</h2>
+    <div id="map" class="map"></div>
+    <script type="text/javascript">
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([37.41, 8.82]),
+          zoom: 4
+        })
+      });
+    </script>
+  ?>
+
+
