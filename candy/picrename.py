953219db611e118ebf45af1c39cf8bbcd8b28ff2
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2022-05-23 21:50:58 +0200

    candy

--- candy/picrename.py
+++ candy/picrename.py
@@ -7,24 +7,29 @@ Created on Mon May 23 19:42:02 2022
 """
 
 import sys
-from PIL import Image, ImageDraw
-
+from PIL import Image, ImageDraw, ImageFont
+import glob
+piclist=glob.glob('*.png')
+print(piclist)
 # get an image
-with Image.open("Monty.png").convert("RGBA") as base:
+for i in piclist:
+    with Image.open(i).convert("RGBA") as base:
+        date= i.replace('.png', '')
+        # make a blank image for the text, initialized to transparent text color
+        txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
 
-    # make a blank image for the text, initialized to transparent text color
-    txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
+        bs= base.size
+        print (bs[0])
+        # get a font
+        fnt = ImageFont.truetype("Quicksand-Regular.ttf", 30)
+        # get a drawing context
+        d = ImageDraw.Draw(txt)
 
-    # get a font
-    fnt = ImageFont.truetype("Quicksand-Regular.ttf", 40)
-    # get a drawing context
-    d = ImageDraw.Draw(txt)
+        # draw text, half opacity
+        d.text((bs[0]-150, bs[1] -40), date, font=fnt, fill=(255, 255, 255, 255))
+        # draw text, full opacity
 
-    # draw text, half opacity
-    d.text((10, 10), "Hello", font=fnt, fill=(255, 255, 255, 128))
-    # draw text, full opacity
-    d.text((10, 60), "World", font=fnt, fill=(255, 255, 255, 255))
+        out = Image.alpha_composite(base, txt)
 
-    out = Image.alpha_composite(base, txt)
-
-    out.show()
+        out.show()
+        out.save(i)
